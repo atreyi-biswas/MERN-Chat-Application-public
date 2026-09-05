a760147ab9e08207e8ac9ae19809cb5aed581762
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-09-05 22:29:35 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,54 @@
 import express from "express";
-import "dotenv/config";
-import dns from "dns";
 import cors from "cors";
+
+import "dotenv/config";
+
+import fs from "fs";
+import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user_model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
-// Use Google DNS for MongoDB Atlas SRV lookup
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-const app = express();
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-// Middleware
+const publicDir = path.join(process.cwd(), "public");
+
+// it's important that you don't parse the webhook event data, it should be in the raw format
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
+
 app.use(express.json());
-
-app.use(
-    cors({
-        origin: FRONTEND_URL,
-        credentials: true,
-    })
-);
-
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
-// Health check
 app.get("/health", (req, res) => {
-    res.status(200).json({ ok: true });
+  res.status(200).json({ ok: true });
 });
 
-// Start server
-app.listen(PORT, async () => {
-    try {
-        await connectDB();
-        console.log("Server is up and running on PORT:", PORT);
-    } catch (error) {
-        console.error("Failed to start server:", error);
-    }
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
+
+server.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
+
+  if (process.env.NODE_ENV === "production") job.start();
 });
