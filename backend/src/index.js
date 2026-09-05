f88d4f0252294feb98a5f57802be6f0ef5afa974
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-09-05 14:01:59 +0530

    update

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+//const express = require('express');  --not used because: 
+
+import express from 'express';
+import "dotenv/config";
+
+import User from "./models/user_model.js";
+import {connectDB} from "./lib/db.js";
+
+
+const app = express();
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json())
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+
+    res.status(200).json({ok: "true"});
+});
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is up and running on PORT:", PORT);
+});
