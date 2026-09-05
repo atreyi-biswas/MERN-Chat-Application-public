6a534949b579dda293d063d6bee3ed5a96b10425
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-09-05 11:00:53 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
-//const express = require('express');
+//const express = require('express');  --not used because: 
 
 import express from 'express';
 const app = express();
 
-app.listen(3000, () => {console.log('Server is up and running on port 3000');});
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log("Server is up and running on PORT:", PORT));
